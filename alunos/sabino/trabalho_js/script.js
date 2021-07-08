361b3b055aa563e56de6d3a9580c2be0f81971ff
SabinoWS <sabino.th@hotmail.com>
2021-07-08 20:11:18 -0300

    Ajusta exemplos JS e adiciona exemplo de teste unitário

--- alunos/sabino/trabalho_js/script.js
+++ alunos/sabino/trabalho_js/script.js
@@ -1,6 +1,25 @@
 
+// Constante para utilizar na requisição da chamada da API
 const BASE_URL = "https://pokeapi.co/api/v2/"
 
+// =============================================================
+// Método para ser chamado ao click do botão
+async function buscarPokemon() {
+    // 1 - (Input) Pegar elementos e valores da tela para utilizar na busca
+    const inputNomePokemon = document.getElementById("nomePokemon")
+    const nomePokemon = inputNomePokemon.value
+
+    // 2 - (Processamento) Chamar função que fará a requisição ajax, enviando o valor do input como parametro
+    const result = await httpGet("pokemon/" + nomePokemon)
+    const imageUrl = result.sprites.front_default
+
+    // 3 - (Output) Alterar os elementos HTML para disponibilizar o resultado em tela
+    let imagemElement = document.getElementById("imagemPokemon")
+    imagemElement.src = imageUrl
+}
+
+// =============================================================
+// Método para realizar chamada Ajax
 async function httpGet(resource) {
     let result
     await $.ajax({
@@ -12,15 +31,4 @@ async function httpGet(resource) {
         }
     });
     return result
-}
-
-async function buscarPokemon() {
-    const inputNomePokemon = document.getElementById("nomePokemon")
-    const nomePokemon = inputNomePokemon.value
-
-    const result = await httpGet("pokemon/" + nomePokemon)
-    const imageUrl = result.sprites.front_default
-
-    let imagemElement = document.getElementById("imagemPokemon")
-    imagemElement.src = imageUrl
 }
